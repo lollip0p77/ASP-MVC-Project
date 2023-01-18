6a16783c577362eb1012e92bd634d3a69705abe1
lollip0p77 <keven.beaulieu.77@outlook.com>
2023-01-18 11:57:26 -0500

    Premier commit

---
+++ ASPMVCSitePandas/ASP(MVC)SitePandas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP_MVC_SitePandas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
